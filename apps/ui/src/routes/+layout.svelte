0ea088e75006612d29202660e7be947e7e110dab
Simon Emms <simon@simonemms.com>
2026-05-12 15:07:06 +0000

    feat: make it easy to change provider name and branding
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- apps/ui/src/routes/+layout.svelte
+++ apps/ui/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import favicon from '$lib/assets/favicon.svg';
+  import logo from '$lib/assets/logo.svg';
 
   import '../app.css';
 
@@ -7,7 +7,7 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href={logo} />
 </svelte:head>
 
 {@render children()}
